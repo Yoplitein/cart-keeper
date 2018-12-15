b8695604ad2025eed4f7b368dd3bdebc64758ffb
Steven Dwy <yoplitein+git@gmail.com>
2018-12-15 15:58:05 -0800

    Gracefully handle uncaught exceptions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
-from flask import Flask
+import traceback
+
+from flask import Flask, jsonify
+
+def handleUncaughtException(error):
+    traceback.print_exception(type(error), error, error.__traceback__)
+    
+    return jsonify(error="Uncaught exception"), 500
 
 def make_app():
     from ckb.views import root
@@ -6,10 +13,11 @@ def make_app():
     app = Flask("cart-keeper")
     
     app.register_blueprint(root)
+    app.register_error_handler(Exception, handleUncaughtException)
     
     return app
 
 app = make_app()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
