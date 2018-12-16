be854af9cb9236f34ff60855ee6eba933e077f62
Steven Dwy <yoplitein+git@gmail.com>
2018-12-15 16:00:08 -0800

    Relax CORS restrictions when debugging

--- src/main.py
+++ src/main.py
@@ -2,6 +2,12 @@ import traceback
 
 from flask import Flask, jsonify
 
+def setCorsHeader(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "content-type"
+    
+    return response
+
 def handleUncaughtException(error):
     traceback.print_exception(type(error), error, error.__traceback__)
     
@@ -11,9 +17,13 @@ def make_app():
     from ckb.views import root
     
     app = Flask("ckb")
+    app.debug = __name__ == "__main__"
     
     app.register_blueprint(root)
     app.register_error_handler(Exception, handleUncaughtException)
+    
+    if app.debug:
+        app.after_request(setCorsHeader)
     
     return app
 
